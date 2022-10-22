e194df553e69449df4543264b349168b2fada0e0
Akihiro <aki19960517@gmail.com>
2022-10-22 18:39:21 +0900

    add: Glassmorphismを配置した。

--- regina/regina/reginaApp.swift
+++ regina/regina/reginaApp.swift
@@ -11,7 +11,12 @@ import SwiftUI
 struct reginaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ZStack{
+                Color.blue
+                    .edgesIgnoringSafeArea(.all)
+                
+                GlassBackGround()
+            }
         }
     }
 }
