e24c6a6e00c201921b6b0e8d0595463faf8a6660
Akihiro <aki19960517@gmail.com>
2022-10-22 17:45:01 +0900

    add: create new project

---
+++ regina/regina/reginaApp.swift
@@ -0,0 +1,17 @@
+//
+//  reginaApp.swift
+//  regina
+//
+//  Created by 伊原明宏 on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct reginaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
